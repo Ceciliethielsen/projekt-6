65f1ff79a6c881d1438f5953c405e4bd51941bed
Ceciliethielsen <cecilie.mft@hotmail.com>
2022-05-24 23:02:32 +0200

    JS kommentarer tilføjet

--- js/kontakt.js
+++ js/kontakt.js
@@ -1,5 +1,7 @@
 //Cecilies JS
 //alert("HELLO WORLD!");
+
+//Variablerne defineres herunder - hentet fra DOM
     let navn = document.getElementById("navn");
     let mail = document.getElementById("mail");
     let tlf = document.getElementById("tlf");
@@ -7,12 +9,16 @@
     let besked = document.getElementById("besked");
     let error = document.getElementById("error");
 
+//Her bliver arrays'ne defineret i forbindelse med en variabel/i en variabel
     let borderColors = ["2px solid red", "1px solid green"];
     let errorMessages = ["Udfyld venligst alle oplysninger", "Mail, telefon, emne og besked mangler", "Telefon, emne og besked mangler", "Emne og besked mangler", "Besked mangler", "Din besked er sendt"];
     let colors = ["red", "black"];
 
+//Sørger for, at kodn først bliver læst, når der trykkes på knappen "send"
     function check(){
+        //if-statements eksekverer en kode, afhængig af betingelsen
         if(navn.value.length ==0 && mail.value.length ==0 && tlf.value.length ==0 && emne.value.length ==0 && besked.value.length ==0){
+            //følgende kalder på arrayet ovenover - herunder henter den 0-værdi'en
             navn.style.border = borderColors[0];
             mail.style.border = borderColors[0];
             tlf.style.border = borderColors[0];
@@ -21,7 +27,7 @@
             error.style.color = colors[0];
             error.innerHTML = errorMessages[0];
         }
-
+        //else-if-statements eksekverer et bestemt stykke kode, afhængig af, om indholdet er delvist sandt eller ej - det er defineret i else-fi()
         else if (navn.value.length !=0 && mail.value.length ==0 && tlf.value.length==0 && emne.value.length ==0 && besked.value.length ==0){
             error.innerHTML = errorMessages[1];
             error.style.color = colors[0];
@@ -69,19 +75,21 @@
             besked.style.border = borderColors[1];
             
         }
+    //Kører console.log, så hvis intet virker, kan det ses i "undersøg/inspect"
         else{
             console.log("Det virker overhovedet ikke")
         }
     }
-
+    //for-loop som fortæller, at i = 0. Det betyder, at så længe i er mindre end array'et "errorMessages", så vil loopet altid blive plusset med 1 - i++
     for (i = 0; i < errorMessages.length; i++){
         console.log(errorMessages[i])
     }
-
+    //Sørger for at siden ikke re-loader hver gang man trykker "send"
     const form = document.getElementById("form-contact");
     function handleform(event){
         console.log("stopreload")
         event.preventDefault();
     }
     form.addEventListener('submit', handleform);
+//Cecilies JS slut
 
--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 //Cecilie JS start
-  //Herunder er burgermenu lavet ved brug af "getElementById", som er hentet fra DOM.
+  
+  //variablerne er defineret herunder - hentet fra DOM
   let burgerMenu = document.getElementById('burgermenu');
   let overlay = document.getElementById('menu');
+
+  //clickfunktion på burgermenuen er aktiveret via et event og en funktion starter
   burgerMenu.addEventListener('click',function(){
+    //toggle er en skifte-funktion, som bruges til at skjule og vise de valgte elementer - her både "close" og "overlay"
     this.classList.toggle("close");
     overlay.classList.toggle("overlay");
   });
