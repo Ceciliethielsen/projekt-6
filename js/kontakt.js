2e8b42e60d8cfb60811133b1c825e63880602c6e
Ceciliethielsen <cecilie.mft@hotmail.com>
2022-05-26 15:02:20 +0200

    kontakt.js opdateret
    Giver bedre mening uden bordercolors - de passer alligevel ikke 100p

--- js/kontakt.js
+++ js/kontakt.js
@@ -32,39 +32,23 @@
         else if (navn.value.length !=0 && mail.value.length ==0 && tlf.value.length==0 && emne.value.length ==0 && besked.value.length ==0){
             error.innerHTML = errorMessages[1];
             error.style.color = colors[0];
-            navn.style.border = borderColors[1];
-            mail.style.border = borderColors[0];
-            tlf.style.border = borderColors[0];
-            emne.style.border = borderColors[0];
-            besked.style.border = borderColors[0];
+            
         }
 
         else if(navn.value.length !=0 && mail.value.length !=0 && tlf.value.length ==0 && emne.value.length ==0 && besked.value.length ==0){
             error.innerHTML = errorMessages[2];
             error.style.color = colors[0];
-            navn.style.border = borderColors[1];
-            mail.style.border = borderColors[1];
-            tlf.style.border = borderColors[0];
-            emne.style.border = borderColors[0];
-            besked.style.border = borderColors[0];
+           
         }
         else if(navn.value.length !=0 && mail.value.length !=0 && tlf.value.length !=0 && emne.value.length ==0 && besked.value.length ==0){
             error.innerHTML = errorMessages[3];
             error.style.color = colors[0];
-            navn.style.border = borderColors[1];
-            mail.style.border = borderColors[1];
-            tlf.style.border = borderColors[1];
-            emne.style.border = borderColors[0];
-            besked.style.border = borderColors[0];
+            
         }
         else if(navn.value.length !=0 && mail.value.length !=0 && tlf.value.length !=0 && emne.value.length !=0 && besked.value.length ==0){
             error.innerHTML = errorMessages[4];
             error.style.color = colors[0];
-            navn.style.border = borderColors[1];
-            mail.style.border = borderColors[1];
-            tlf.style.border = borderColors[1];
-            emne.style.border = borderColors[1];
-            besked.style.border = borderColors[0];
+          
         }
         else if(navn.value.length !=0 && mail.value.length !=0 && tlf.value.length !=0 && emne.value.length !=0 && besked.value.length == true){
             error.innerHTML = errorMessages[5];
